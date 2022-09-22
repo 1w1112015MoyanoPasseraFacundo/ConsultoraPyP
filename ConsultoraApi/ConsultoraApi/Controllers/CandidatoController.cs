a46fb31117af65326c0ad96c1730eba46be3554d
Facu Moyano <fmoyano@encodesa.com.ar>
2022-09-22 14:20:22 -0300

    gets

---
+++ ConsultoraApi/ConsultoraApi/Controllers/CandidatoController.cs
@@ -0,0 +1,34 @@
+﻿using ConsultoraApi.Models;
+using ConsultoraApi.Resultados;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsultoraApi.Controllers
+{
+    [ApiController]
+    [EnableCors("ConsultoraApi")]
+    public class CandidatoController
+    {
+        private readonly ConsultoraPypContext db = new ConsultoraPypContext();
+        [HttpGet]
+        [Route("candidatos")]
+        public ActionResult<RespuestaAPI> Get()
+        {
+            var resultado = new RespuestaAPI();
+            try
+            {
+                resultado.Ok = true;
+                resultado.Respuesta = db.Candidatos.ToList();
+
+                return resultado;
+            }
+            catch (Exception ex)
+            {
+                resultado.Ok = false;
+                resultado.Error = "Error al encontrar candidatos";
+                return resultado;
+            }
+
+        }
+    }
+}
---
+++ ConsultoraApi/ConsultoraApi/Controllers/CompetenciaController.cs
@@ -0,0 +1,34 @@
+﻿using ConsultoraApi.Models;
+using ConsultoraApi.Resultados;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsultoraApi.Controllers
+{
+    [ApiController]
+    [EnableCors("ConsultoraApi")]
+    public class CompetenciaController
+    {
+        private readonly ConsultoraPypContext db = new ConsultoraPypContext();
+        [HttpGet]
+        [Route("competencias")]
+        public ActionResult<RespuestaAPI> Get()
+        {
+            var resultado = new RespuestaAPI();
+            try
+            {
+                resultado.Ok = true;
+                resultado.Respuesta = db.Competencias.ToList();
+
+                return resultado;
+            }
+            catch (Exception ex)
+            {
+                resultado.Ok = false;
+                resultado.Error = "Error al encontrar competencias";
+                return resultado;
+            }
+
+        }
+    }
+}
